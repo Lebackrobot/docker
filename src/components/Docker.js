f56fa3159e523cbc0ca6073f880bfa83bfb2fae9
Lebackrobot <lebackman18@gmail.com>
2024-01-18 15:07:50 -0300

    react build projetct

---
+++ src/components/Docker.js
@@ -0,0 +1,14 @@
+import './Docker.css'
+import dockerImage from '../images/icon.png'
+
+const Docker = () => {
+    return (
+        <div>
+            <img src={ dockerImage } alt='Icon docker'/>
+            <h1>I ❤️ Docker!</h1>
+        </div>
+    )   
+}
+
+export default Docker
+
