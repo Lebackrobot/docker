73d509f65bb4276273d46d9681719bfb53460633
Lebackrobot <lebackman18@gmail.com>
2024-02-02 15:00:23 -0300

    feature: loading component

--- src/components/Docker.js
+++ src/components/Docker.js
@@ -1,20 +1,36 @@
 import React, { useEffect, useState} from 'react'
 import './Docker.css'
+
 import dockerImage from '../images/icon.png'
+import loadingImage from '../images/loading.gif'
 
 import dockerApi from '../apis/docker-api'
 
 const Docker = () => {
     const [message, setMessage] = useState()
-    const id = 1
+    const [loading, setLoading] = useState(true);
+    const id = 1;
 
     useEffect(() => {
         dockerApi
             .get(`/message/${id}`)
-            .then(response => setMessage(response.data))
+            .then(response => {
+                setMessage(response.data)
+                setTimeout(() => {
+                    setLoading(false)    
+                }, 2000)
+            })
             .catch(err => console.error(`An error has occurred: ${err}`))
     }, [])
 
+    if (loading) {
+        return (
+            <div>
+                <img src={ loadingImage } alt='loading image'/>
+            </div>
+        )
+    }
+    
     return (
         <div>
             <img src={ dockerImage } alt='Icon docker'/>
