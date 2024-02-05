1be3b58cfef13c1c9c0bedc46bdf1cad1f927c3e
Lebackrobot <lebackman18@gmail.com>
2024-02-05 19:03:01 -0300

    feat: add info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Docker from './components/Docker'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Docker></Docker>
 
+      <footer>
+
+        <span style={{ color: '#04131c' }}>
+          by <a style={{ color: 'inherit', textDecoration: '' }} href='https://github.com/Lebackrobot'>Pedro Fernandes</a>
+        </span>
+      </footer>
     </div>
+    
   );
 }
 
